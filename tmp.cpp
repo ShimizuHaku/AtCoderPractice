783adfd24dc5838aa837139b15712581c77e23af
ShimizuHaku <shimizu.haku89@gmail.com>
2024-04-03 01:44:54 +0900

    Create tmp.cpp

---
+++ tmp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <regex>
+#include <deque>
+#include <numeric>
+#include <iterator>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+}
