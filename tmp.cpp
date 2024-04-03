052ef04424c7e232749010d1bf1c51fb219d1bc2
ShimizuHaku <shimizu.haku89@gmail.com>
2024-04-04 02:51:49 +0900

    Update tmp.cpp

--- tmp.cpp
+++ tmp.cpp
@@ -1,19 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <utility>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <queue>
-#include <stack>
-#include <cmath>
-#include <regex>
-#include <deque>
-#include <numeric>
-#include <iterator>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
